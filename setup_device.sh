896b4faed33c81ee15f38ff01470540ddf17a9c5
Aksh Desai <dev.akshdesai@gmail.com>
2025-09-26 16:24:19 +0530

    Added Models to pull command

--- setup_device.sh
+++ setup_device.sh
@@ -67,6 +67,23 @@ sudo snap install postman
 echo "Installing Ollama..."
 curl -fsSL https://ollama.com/install.sh | sh
 
+# Embedding Models
+ollama pull embeddinggemma
+ollama pull nomic-embed-text
+ollama pull mxbai-embed-large
+
+# Reasoning Models
+ollama pull deepseek-r1:1.5b
+ollama pull deepseek-r1:7b
+
+# Coding Models
+ollama pull qwen2.5-coder:3b
+ollama pull qwen2.5-coder:7b
+
+# Gemeral Models
+ollama pull gemma3:4b
+ollama pull gemma3:12b
+
 # AWS CLI
 echo "Installing AWS CLI..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
