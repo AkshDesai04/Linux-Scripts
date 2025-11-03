f880cbaf0ebf50f4487f745bbe9d32514082dec9
Aksh Desai <aksh.d.4002@gmail.com>
2025-11-03 21:38:31 +0530

    User Setup Updates

--- dev_langs_comps.sh
+++ dev_langs_comps.sh
@@ -1,2 +1,12 @@
 set +e
-sudo apt install -y build-essential python3 python3-pip python3-venv gcc g++ make cmake default-jdk npm clang golang-go ruby-full php rustc cargo nodejs automake
+sudo apt install -y build-essential
+sudo apt install -y python3 python3-pip python3-venv
+sudo apt install -y gcc g++ make cmake clang automake
+sudo apt install -y default-jdk
+sudo apt install -y npm
+sudo apt install -y golang-go
+sudo apt install -y ruby-full
+sudo apt install -y php
+sudo apt install -y rustc
+sudo apt install -y cargo
+sudo apt install -y nodejs
