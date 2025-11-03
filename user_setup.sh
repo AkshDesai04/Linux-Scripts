71de6de06b974c31063c7335ff1a403d8e1fdafa
Aksh Desai <aksh.d.4002@gmail.com>
2025-11-03 15:11:55 +0530

    Added personalization touches

---
+++ user_setup.sh
@@ -0,0 +1,57 @@
+GITHUB_USER="AkshDesai04"
+WALLPAPER_PATH="./wallpaper.jpg"
+TEMP_PIC_PATH="/tmp/github_profile_pic.png"
+
+echo "Fetching profile picture for '$GITHUB_USER'..."
+
+AVATAR_URL=$(curl -s "https://api.github.com/users/$GITHUB_USER" | grep 'avatar_url' | cut -d '"' -f 4)
+
+if [ -z "$AVATAR_URL" ]; then
+    echo "Error: Could not fetch avatar URL for user '$GITHUB_USER'."
+    exit 1
+fi
+
+echo "Downloading profile picture..."
+curl -s -L "$AVATAR_URL" -o "$TEMP_PIC_PATH"
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to download profile picture."
+    exit 1
+fi
+
+echo "Setting profile picture..."
+dbus-send --system --dest=org.freedesktop.Accounts \
+    --type=method_call --print-reply \
+    /org/freedesktop/Accounts/User$(id -u) \
+    org.freedesktop.Accounts.User.SetIconFile \
+    string:"$TEMP_PIC_PATH"
+
+if [ $? -ne 0 ]; then
+    echo "Warning: Failed to set profile picture via D-Bus."
+    echo "As a fallback, copying to ~/.face (may require login/logout to see)."
+    cp "$TEMP_PIC_PATH" "$HOME/.face"
+else
+    echo "Profile picture set successfully."
+fi
+
+rm "$TEMP_PIC_PATH"
+
+echo "Setting wallpaper..."
+if [ ! -f "$WALLPAPER_PATH" ]; then
+    echo "Error: Wallpaper file not found at $WALLPAPER_PATH"
+    echo "Please make sure 'wallpaper.png' is in the same directory as this script."
+    exit 1
+fi
+
+ABS_WALLPAPER_PATH=$(readlink -f "$WALLPAPER_PATH")
+WALLPAPER_URI="file://$ABS_WALLPAPER_PATH"
+
+echo "Setting wallpaper to $WALLPAPER_URI"
+
+gsettings set org.gnome.desktop.background picture-uri "$WALLPAPER_URI"
+gsettings set org.gnome.desktop.background picture-uri-dark "$WALLPAPER_URI"
+
+gsettings set org.gnome.desktop.background picture-options 'zoomed'
+
+echo "Wallpaper set successfully."
+echo "Done."
