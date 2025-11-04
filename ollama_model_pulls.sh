e80c23b4529da40242adf8799e26ed4cd27ea084
Aksh Desai <aksh.d.4002@gmail.com>
2025-11-04 14:28:00 +0530

    Added better comments and indentation

--- ollama_model_pulls.sh
+++ ollama_model_pulls.sh
@@ -1,10 +1,18 @@
 set +e
+
+# Embedding Models
 ollama pull embeddinggemma
 ollama pull nomic-embed-text
-ollama pull mxbai-embed-large
+#ollama pull mxbai-embed-large
+
+# Reasoning Models
 ollama pull deepseek-r1:1.5b
 ollama pull deepseek-r1:7b
+
+# Coding Models
 ollama pull qwen2.5-coder:3b
 ollama pull qwen2.5-coder:7b
+
+# Gemeral Models
 ollama pull gemma3:4b
 ollama pull gemma3:12b
